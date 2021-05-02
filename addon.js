223e3a1869eb091ce4a99e911268360bc03500d7
Elandig <15586732+Elandig@users.noreply.github.com>
2021-05-02 19:12:44 +0300

    Fixed tabulation

--- addon.js
+++ addon.js
@@ -16,7 +16,7 @@ const init = () => {
 const printSequence = (obj, amount = 10) => {
   let sequence = []
   for (let i=0; i < amount; i++)
-  sequence.push(obj.get())
+    sequence.push(obj.get())
 
   console.log(`Should output the first ${amount} numbers of the fibonacci sequence: ${sequence.join()}`)
 }
