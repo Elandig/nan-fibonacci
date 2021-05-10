2e2f0559781ec53959bfc0397cb7042008f5b3fc
Elandig <15586732+Elandig@users.noreply.github.com>
2021-05-10 04:25:43 +0300

    Updated tests

--- addon.js
+++ addon.js
@@ -16,17 +16,26 @@ const init = () => {
   instance.reset() ? console.log('Sequence was reset!') : ''
   printSequence(instance)
 
-  console.log(`\n* Check method .isFibo()`)
+  console.log(`\n* Check method .genFibo(number)`)
+  console.log(`Should output the first 20 numbers of the fibonacci sequence: ${instance.genFibo(20)}`)
+
+  console.log(`\n* Check method .isFibo(string | number)`)
   checkIsFibonacci();
 }
 
+/*
+ * Generates and prints the sequence using internal accumulator
+ */
 const printSequence = (obj, amount = 10) => {
-  for (let i=0; i < amount-1; i++)
+  for (let i = 0; i < amount - 1; i++)
     obj.next()
 
   console.log(`Should output the first ${amount} numbers of the fibonacci sequence: ${obj.get()}`)
 }
 
+/*
+ * Checks if the given values are part of the fibonacci sequence
+ */
 const checkIsFibonacci = () => {
   console.log(`Should output true (String): ${instance.isFibo("280571172992510140037611932413038677189525")}`)
   console.log(`Should output true (Max value for Number): ${instance.isFibo(8944394323791464)}`)
